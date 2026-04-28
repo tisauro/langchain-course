d91704c3730a26d9cca3fd820eefc3367d1040a7
tisauro <tisauro@gmail.com>
2026-04-28 15:05:48 +0100

    + tavily search agent

--- main.py
+++ main.py
@@ -3,35 +3,22 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
 
-tavily = TavilyClient()
 
 
-@tool
-def search(query: str) -> str:
-    """
-    Performs a search over the internet based on the provided query and returns the relevant result.
-
-    :param query: The search term or query used for the operation.
-    :type query: str
-    :return: The result of the search operation as a string.
-    :rtype: str
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
 
 
 llm = ChatOpenAI(model="gpt-5")
-tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages": HumanMessage(content="Search for job postings for IoT Engineer (Python, Edge-to-Cloud Systems) in the Tadworth area and list all their details")})
+    result = agent.invoke({"messages": HumanMessage(content="Search for remote job postings for IoT Engineer (Python, Edge-to-Cloud Systems) and list all their details")})
     print(result)
 
 
